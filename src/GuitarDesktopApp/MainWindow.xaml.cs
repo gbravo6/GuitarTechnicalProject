50fa9f7a65db5b575f869029b23229058520f8ab
Griffin Bravo <144168830+gbravo6@users.noreply.github.com>
2025-03-13 16:50:12 -0600

    desktop app experimentation

--- src/GuitarDesktopApp/MainWindow.xaml.cs
+++ src/GuitarDesktopApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
 
 namespace GuitarDesktopApp
 {
@@ -16,9 +19,38 @@ namespace GuitarDesktopApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        //fields 
+        BluetoothClient btc = null;
+        BluetoothDeviceInfo[] devices = null;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void UI_Scan_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            UI_Devices_List.Items.Clear();
+            //scan for devices
+            try
+            {
+
+                btc = new BluetoothClient();
+                devices = btc.DiscoverDevices();
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine($"Scanning Devices Failed: {ex.Message}");
+                UI_Devices_List.Items.Add("Scanning Devices Failed.");
+            }
+            foreach (BluetoothDeviceInfo device in devices)
+            {
+                UI_Devices_List.Items.Add($"Device Name: {device.DeviceName} | Address: {device.DeviceAddress}");
+            }
+        }
+
+        private void ToPage1_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Content = new Page1();
         }
     }
 }
